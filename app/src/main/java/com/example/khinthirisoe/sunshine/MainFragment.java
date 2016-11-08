3d07a091ac3bacaf8ab9b46cb32733d7196d19e3
Khin Thiri Soe <khinthirisoe.mdy@gmail.com>
2016-11-08 17:33:08 +0630

    populate list item data with mock data

--- app/src/main/java/com/example/khinthirisoe/sunshine/MainFragment.java
+++ app/src/main/java/com/example/khinthirisoe/sunshine/MainFragment.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public MainFragment() {
     }
@@ -17,6 +23,23 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] mForecastWeather = {
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+        };
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, mForecastWeather);
+        listView.setAdapter(mForecastAdapter);
+
+        return view;
+
     }
 }
